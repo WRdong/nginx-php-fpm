b3d2becf8ceca5e65b6208af6dcc40187d5b0165
tcl-80 <root@tcl-80.com>
2018-04-30 08:01:34 +0800

    update mysql 5.7 yml

--- webroot/phpmyadmin/config.inc.php
+++ webroot/phpmyadmin/config.inc.php
@@ -28,7 +28,7 @@ $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
-$cfg['Servers'][$i]['host'] = '10.104.10.37';
+$cfg['Servers'][$i]['host'] = 'mysql';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
 
