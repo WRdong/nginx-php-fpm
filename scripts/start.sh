a870a86edad7b1f4bbaa4d0a9567230830b10e7d
WRdong <527184984@qq.com>
2018-09-03 17:39:16 +0800

    增加 ll 别名 ll -> ls -l, 启动 crond

--- scripts/start.sh
+++ scripts/start.sh
@@ -34,35 +34,6 @@ if [ ! -z "$GIT_NAME" ]; then
 fi
 
 # Dont pull code down if the .git folder exists
-if [ ! -d "/var/www/html/.git" ]; then
- # Pull down code from git for our site!
- if [ ! -z "$GIT_REPO" ]; then
-   # Remove the test index file if you are pulling in a git repo
-   if [ ! -z ${REMOVE_FILES} ] && [ ${REMOVE_FILES} == 0 ]; then
-     echo "skiping removal of files"
-   else
-     rm -Rf /var/www/html/*
-   fi
-   GIT_COMMAND='git clone '
-   if [ ! -z "$GIT_BRANCH" ]; then
-     GIT_COMMAND=${GIT_COMMAND}" -b ${GIT_BRANCH}"
-   fi
-
-   if [ -z "$GIT_USERNAME" ] && [ -z "$GIT_PERSONAL_TOKEN" ]; then
-     GIT_COMMAND=${GIT_COMMAND}" ${GIT_REPO}"
-   else
-    if [[ "$GIT_USE_SSH" == "1" ]]; then
-      GIT_COMMAND=${GIT_COMMAND}" ${GIT_REPO}"
-    else
-      GIT_COMMAND=${GIT_COMMAND}" https://${GIT_USERNAME}:${GIT_PERSONAL_TOKEN}@${GIT_REPO}"
-    fi
-   fi
-   ${GIT_COMMAND} /var/www/html || exit 1
-   if [ -z "$SKIP_CHOWN" ]; then
-     chown -Rf nginx.nginx /var/www/html
-   fi
- fi
-fi
 
 # Enable custom nginx config files if they exist
 if [ -f /var/www/html/conf/nginx/nginx.conf ]; then
@@ -138,37 +109,6 @@ if [ ! -z "$PHP_UPLOAD_MAX_FILESIZE" ]; then
  sed -i "s/upload_max_filesize = 100M/upload_max_filesize= ${PHP_UPLOAD_MAX_FILESIZE}M/g" /usr/local/etc/php/conf.d/docker-vars.ini
 fi
 
-# Enable xdebug
-XdebugFile='/usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini'
-if [[ "$ENABLE_XDEBUG" == "1" ]] ; then
-  if [ -f $XdebugFile ]; then
-  	echo "Xdebug enabled"
-  else
-  	echo "Enabling xdebug"
-  	echo "If you get this error, you can safely ignore it: /usr/local/bin/docker-php-ext-enable: line 83: nm: not found"
-  	# see https://github.com/docker-library/php/pull/420
-    docker-php-ext-enable xdebug
-    # see if file exists
-    if [ -f $XdebugFile ]; then
-        # See if file contains xdebug text.
-        if grep -q xdebug.remote_enable "$XdebugFile"; then
-            echo "Xdebug already enabled... skipping"
-        else
-            echo "zend_extension=$(find /usr/local/lib/php/extensions/ -name xdebug.so)" > $XdebugFile # Note, single arrow to overwrite file.
-            echo "xdebug.remote_enable=1 "  >> $XdebugFile
-            echo "xdebug.remote_log=/tmp/xdebug.log"  >> $XdebugFile
-            echo "xdebug.remote_autostart=false "  >> $XdebugFile # I use the xdebug chrome extension instead of using autostart
-            # NOTE: xdebug.remote_host is not needed here if you set an environment variable in docker-compose like so `- XDEBUG_CONFIG=remote_host=192.168.111.27`.
-            #       you also need to set an env var `- PHP_IDE_CONFIG=serverName=docker`
-        fi
-    fi
-  fi
-else
-    if [ -f $XdebugFile ]; then
-        echo "Disabling Xdebug"
-      rm $XdebugFile
-    fi
-fi
 
 if [ ! -z "$PUID" ]; then
   if [ -z "$PGID" ]; then
@@ -193,19 +133,6 @@ if [[ "$RUN_SCRIPTS" == "1" ]] ; then
   else
     echo "Can't find script directory"
   fi
-fi
-
-if [ -z "$SKIP_COMPOSER" ]; then
-    # Try auto install for composer
-    if [ -f "/var/www/html/composer.lock" ]; then
-        if [ "$APPLICATION_ENV" == "development" ]; then
-            composer global require hirak/prestissimo
-            composer install --working-dir=/var/www/html
-        else
-            composer global require hirak/prestissimo
-            composer install --no-dev --working-dir=/var/www/html
-        fi
-    fi
 fi
 
 # Start supervisord and services
